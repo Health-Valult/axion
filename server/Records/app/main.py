2d0ccd3716e6ab8ada02b98f44c899efd65a4263
RavinduJS <110709934+RavinduJS@users.noreply.github.com>
2025-02-15 23:42:28 +0530

    commit-9.0

--- server/Records/app/main.py
+++ server/Records/app/main.py
@@ -3,8 +3,25 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from .models.models import Test
 import boto3
+from strawberry.fastapi import GraphQLRouter
+import strawberry
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def dickweed(self) -> str:
+        return "Hello World"
+
+
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema)
+
+
 
 app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
+
 
 app.post("/axion/data/records/test")
 def records_test(data:Test):
---
+++ server/shared/utils/fhir_reader.py
@@ -0,0 +1,6 @@
+from fhir.resources import patient,allergyintolerance,careplan,careteam,claim,condition,device
+
+class Patient():
+    
+    def __init__(self):
+        pass
