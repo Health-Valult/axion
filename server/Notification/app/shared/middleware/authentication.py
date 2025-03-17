54ad29dcf26202f870f3d625ca71148c22e55650
RavinduJS <110709934+RavinduJS@users.noreply.github.com>
2025-03-17 16:38:49 +0530

    commit - 22.11

--- server/Notification/app/shared/middleware/authentication.py
+++ server/Notification/app/shared/middleware/authentication.py
@@ -26,7 +26,7 @@ def Authenticate_WS(webSocket: WebSocket):
     
     Mq:sendMQ = webSocket.app.state.sender_task 
     token:str = webSocket.headers.get('authorization')
-    print(token)
+    print("logged as mid")
     if token is None:
         webSocket.close()
         raise WebSocketException(code=1008, reason="session token expired or invalid")
