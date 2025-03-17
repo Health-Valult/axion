0d1abe03efd9fe0f7d579e33a7c534a756637fe1
RavinduJS <110709934+RavinduJS@users.noreply.github.com>
2025-03-17 13:38:48 +0530

    commit - 22.4
    bug fix - forgot to add the correct host 😁🔫

--- server/Notification/app/main.py
+++ server/Notification/app/main.py
@@ -67,4 +67,4 @@ app.include_router(r_route)
 
 
 if __name__ == '__main__':
-    uvicorn.run("app.main:app",port=8080,reload=True)
+    uvicorn.run("app.main:app",host="0.0.0.0",port=8080,reload=True)
