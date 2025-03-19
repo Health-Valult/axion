ace924af30f172f98fb99140969727b2ca3c7c8d
RavinduJS <110709934+RavinduJS@users.noreply.github.com>
2025-03-19 23:01:08 +0530

    commit - 24.17

--- server/Records/app/ax_types/immunization.py
+++ server/Records/app/ax_types/immunization.py
@@ -8,12 +8,10 @@ class Immunization:
     patientID: Optional[str]
     code: Optional[str]
     display: Optional[str]
-    status: Optional[str]
     dosage: Optional[str]
     unit: Optional[str]
     site: Optional[str]
     timestamp: Optional[str]
-    manufacturer:Optional[str]
     meta: Optional[Json_type]  # type: ignore
 
 
