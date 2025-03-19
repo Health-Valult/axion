610427b4cefbf609ae774b50402d904c864a5544
RavinduJS <110709934+RavinduJS@users.noreply.github.com>
2025-03-19 11:40:33 +0530

    commit - 24.05

--- server/Security/app/utils/signIn_user.py
+++ server/Security/app/utils/signIn_user.py
@@ -19,7 +19,7 @@ def _sign_in_user(credentials,Collection:Collection,query:dict)->JSONResponse:
         if c_user_exists:
             return JSONResponse(status_code=409, content={"details":"user already exists"})
         
-        credentials["UserID"] = bson.Binary.from_uuid(uuid.uuid5(uuid.NAMESPACE_DNS,credentials.get("Email")))
+        credentials["UserID"] = str(uuid.uuid5(uuid.NAMESPACE_DNS,credentials.get("Email")))
         credentials["prevLogin"]={}
         credentials["Password"] = hasher.hash(credentials.get("Password")) 
 
