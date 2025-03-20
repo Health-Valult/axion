c11ddf5ee9ef166d5c2315649ccad050a98cafe0
RavinduJS <110709934+RavinduJS@users.noreply.github.com>
2025-03-20 17:40:04 +0530

    commit - 25.26

--- server/Security/app/shared/utils/Cache/redis.py
+++ server/Security/app/shared/utils/Cache/redis.py
@@ -102,7 +102,7 @@ class redis_AX:
             message = waiting_channel.get_message()
             
 
-            if message['type'] == 'message':
+            if message.get("type") == 'message':
                 data = message.get("data").decode("utf-8")
                 response = RedRequest.model_validate_json(data)
                 print("u got here")
