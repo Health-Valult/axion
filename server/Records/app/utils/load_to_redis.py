8b81d54676bdce9c30083eea4c93cbc8f29c08fb
RavinduJS <110709934+RavinduJS@users.noreply.github.com>
2025-03-20 21:14:54 +0530

    commit - 25.51

--- server/Records/app/utils/load_to_redis.py
+++ server/Records/app/utils/load_to_redis.py
@@ -1,15 +1,18 @@
 import asyncio
+import logging
 from fastapi import FastAPI
 from pymongo.collection import Collection
 from pymongo.cursor import Cursor
 import redis
+
+logger = logging.getLogger("uvicorn")
 
 
 async def load_to_redis(app:FastAPI):
     collection:Collection = app.state.PatientsCollection
     cache = redis.Redis(host ="cache" ,port=6379)
     patients:Cursor = collection.find({},{"_id": 0, "FirstName": 1, "NIC": 1,"UserID":1 })
-    
+    logger.info("indexing to 🟥is")
     for documents in patients:
         nic = documents.get("NIC")
         
