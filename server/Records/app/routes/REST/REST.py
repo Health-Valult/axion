ae8371ea085228bcfc06c7ca1a51b6587a5b9619
RavinduJS <110709934+RavinduJS@users.noreply.github.com>
2025-03-23 21:41:11 +0530

    commit - 27.39

--- server/Records/app/routes/REST/REST.py
+++ server/Records/app/routes/REST/REST.py
@@ -84,9 +84,9 @@ async def upload_report(request:Request,type:str,report:BaseReportTemplate):
             patientID=patientID,
             labID="",
             value=value,
-            code=meta.json_schema_extra.get("loinc_code"),
-            display=meta.json_schema_extra.get("official_name"),
-            unit=meta.json_schema_extra.get("unit"),
+            code=meta.json_schema_extra.get("metadata").get("loinc_code"),
+            display=meta.json_schema_extra.get("metadata").get("official_name"),
+            unit=meta.json_schema_extra.get("metadata").get("unit"),
             timestamp=timestamp,
             meta={
                 "clinc":clinic,
