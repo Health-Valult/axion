9534a7a3ff33f10fc20e6ea9fd8f8eb4217db8f3
RavinduJS <110709934+RavinduJS@users.noreply.github.com>
2025-03-24 04:18:24 +0530

    commit - 27.59

--- server/Records/app/routes/GQL/Doctor.py
+++ server/Records/app/routes/GQL/Doctor.py
@@ -22,7 +22,7 @@ def get_patient(uuid:str,CACHE:redis_AX):
     patient = CACHE.get_item(name=name)
     if not patient:
         raise Exception("patient not found")
-    return patient.get("patient")
+    return patient
 
 @strawberry.type
 class Query:
